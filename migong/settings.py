15f0c00485c3f1c88497e84e40781618129d0c20
INBUM02 <kib0202@naver.com>
2022-11-12 15:16:30 +0900

    FIX:scret key=>.env

--- migong/settings.py
+++ migong/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v6%aiyr-5ooybxago7*dh8*o+(gzj!6n$q+kk@fz@rli7b5er3'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
